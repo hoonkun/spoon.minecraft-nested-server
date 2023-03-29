65e5f1b3f831cf73fd4ae11ce18a2a9ea740903c
hoonkun <herokun.user@gmail.com>
2023-03-29 23:33:53 +0900

    fix: 마인크래프트 서버 버전을 1.19.4 로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
 
     testImplementation(kotlin("test"))
 
-    implementation("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    implementation("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
 
     implementation("at.favre.lib:bcrypt:0.10.2")
 
